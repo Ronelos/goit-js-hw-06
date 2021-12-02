fdc4faa58981bfcdb73e26c84e8e4770a5db938e
Andrii Kryvyi <87859325+Ronelos@users.noreply.github.com>
2021-12-02 23:10:55 +0200

    hw6-task2-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("ul");
+
+const Potatoes = document.createElement("li");
+Potatoes.classList.add('item');
+Potatoes.textContent = "Potatoes";
+list.append(Potatoes);
+
+const Mushrooms = document.createElement("li");
+Mushrooms.classList.add('item');
+Mushrooms.textContent = "Mushrooms";
+list.append(Mushrooms);
+
+const Garlic = document.createElement("li");
+Garlic.classList.add('item');
+Garlic.textContent = "Garlic";
+list.append(Garlic);
+
+const Tomatos = document.createElement("li");
+Tomatos.classList.add('item');
+Tomatos.textContent = "Tomatos";
+list.append(Tomatos);
+
+const Herbs = document.createElement("li");
+Herbs.classList.add('item');
+Herbs.textContent = "Herbs";
+list.append(Herbs);
+
+const Condiments = document.createElement("li");
+Condiments.classList.add('item');
+Condiments.textContent = "Condiments";
+list.append(Condiments);
